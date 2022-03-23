f0dc913a2706addd38c7633c443803867c605e18
Ember 'n0emis' Keske <git@n0emis.eu>
2022-03-23 14:05:19 +0100

    shorten substring

--- modules/default.nix
+++ modules/default.nix
@@ -9,7 +9,7 @@ let
   mkDomain = name: domCfg:
     let
       cidrToAddress = cidr: head (splitString "/" cidr);
-      mkIfName = type: substring 0 16 (
+      mkIfName = type: substring 0 15 (
         if type == "bridge" then "br-${name}" else
         if type == "batman" then "bat-${name}" else
         if type == "fastd" then "fd-${name}" else
